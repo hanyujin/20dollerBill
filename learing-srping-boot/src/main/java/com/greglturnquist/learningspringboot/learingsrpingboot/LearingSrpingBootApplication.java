5427ef26d4159c7bd6c9aafac615a569587c66b0
YujinHan <godss39822@naver.com>
2019-03-08 22:03:16 +0900

    init

---
+++ learing-srping-boot/src/main/java/com/greglturnquist/learningspringboot/learingsrpingboot/LearingSrpingBootApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.learningspringboot.learingsrpingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearingSrpingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearingSrpingBootApplication.class, args);
+	}
+
+}
